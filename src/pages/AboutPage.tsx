96bc3f08b96f20821434f4bd5d283e7b2060f0bd
Pham Kiet <84656881+Henlla@users.noreply.github.com>
2024-06-10 10:06:45 +0700

    Revert "Product Home"

--- src/pages/AboutPage.tsx
+++
@@ -1,7 +0,0 @@
-const AboutPage = () => {
-    return (<>
-        <h1 className="text-center md:h-screen">This is about page</h1>
-    </>);
-}
-
-export default AboutPage;
--- src/pages/ContactPage.tsx
+++
@@ -1,5 +0,0 @@
-const ContactPage = () => {
-    return (<><h1 className="text-center">This is contact page</h1></>);
-}
-
-export default ContactPage;
