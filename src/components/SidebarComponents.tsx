0254f0c60824b4a65f339408cdb493b8b3c08774
Pham Kiet <phamkiet2234@gmail.com>
2024-06-10 10:11:12 +0700

    Update SidebarComponents.tsx

--- src/components/SidebarComponents.tsx
+++ src/components/SidebarComponents.tsx
@@ -83,7 +83,7 @@ export default function SidebarComponents() {
             if (result.isSuccess)
                 setProducts(result.data)
         }
-        getProductData
+        getProductData();
     }, [])
 
     return (
