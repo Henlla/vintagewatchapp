bc7501f5b9f0b061a759654f398e65cf41821b97
Pham Kiet <phamkiet2234@gmail.com>
2024-06-13 22:52:12 +0700

    Update timepiece

--- src/components/SignInComponents.tsx
+++ src/components/SignInComponents.tsx
@@ -55,16 +55,16 @@ function SignInComponents() {
     };
 
     return (
-        <section className="bg-gray-50 dark:bg-gray-900">
-            <div style={{ height: "76vh" }} className="flex flex-col items-center justify-center px-6 py-8 mt-20 lg:py-0">
-                <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+        <section className="bg-gray-50 md:h-screen pt-12 dark:bg-gray-900">
+            <div style={{ height: "76vh" }} className="flex flex-col items-center justify-center px-6 py-8 lg:py-0">
+                {/* <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
                     <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo" />
                     Vintage Timepiece
-                </a>
+                </a> */}
                 <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                     <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                         <h1 className="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                            Sign in to your account
+                            Log in to your account
                         </h1>
 
 
--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,5 +1,5 @@
 const ContactPage = () => {
-    return (<><h1 className="text-center">This is contact page</h1></>);
+    return (<><h1 className="text-center md:h-screen">This is contact page</h1></>);
 }
 
 export default ContactPage;
