0c1de123cc5112841ebe2c758b309a5f904c9c60
Vũ Đậu Hoàng <hoangvdt.dev@gmail.com>
2024-06-24 17:49:20 +0700

    updataDashboard

---
+++ src/components/DashBoard/Admin/ManageAccount.jsx
@@ -0,0 +1,231 @@
+import { Button, Form, Input, Modal, Table } from "antd";
+import { useForm } from "antd/es/form/Form";
+import React, { useEffect, useState } from "react";
+import { message } from "antd";
+
+export const ManageAccount = () => {
+  const [accounts, setAccounts] = useState([]);
+  const [form] = useForm();
+
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "username",
+      filterMode: "tree",
+      filterSearch: true,
+      onFilter: (value, record) => record.name.includes(value),
+      width: "30%",
+    },
+    {
+      title: "Mail",
+      dataIndex: "email",
+
+      onFilter: (value, record) => record.mail.startsWith(value),
+      filterSearch: true,
+      width: "20%",
+    },
+
+    {
+      title: "Phone",
+      dataIndex: "phone",
+
+      onFilter: (value, record) => record.phone.startsWith(value),
+      filterSearch: true,
+      width: "20%",
+    },
+
+    {
+      title: "Adress",
+      dataIndex: "address",
+
+      onFilter: (value, record) => record.address.startsWith(value),
+      filterSearch: true,
+      width: "20%",
+    },
+
+    {
+      title: "Role",
+      dataIndex: "role",
+
+      onFilter: (value, record) => record.address.startsWith(value),
+      filterSearch: true,
+      width: "10%",
+    },
+
+    {
+      title: "Status",
+      dataIndex: "status",
+
+      onFilter: (value, record) => record.status.startsWith(value),
+      filterSearch: true,
+      width: "20%",
+    },
+    // {
+    //   title: "Actions",
+    //   render: (value, record) => (
+    //     <Button type="dashed" onClick={() => handleDelete(record.id)}>
+    //       Xóa
+    //     </Button>
+    //   ),
+    // },
+  ];
+
+  const onChange = () => {};
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    // setIsModalOpen(false);
+    form.submit();
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleChange = (value) => {
+    console.log(`selected ${value}`);
+  };
+
+  const fetchAccounts = async () => {
+    try {
+      const response = await api.get("/authentication/accounts");
+      const filteredAccounts = response.data.filter(
+        (product) => !product.deleted
+      );
+      setAccounts(filteredAccounts);
+    } catch (error) {
+      console.error("Error fetching account:", error);
+      message.error("Failed to fetch account");
+    }
+  };
+
+  useEffect(() => {
+    fetchAccounts();
+  }, []);
+
+  const onSubmit = async (values) => {
+    console.log(values);
+    const response = await api.post("/authentication/register-staff", values);
+    console.log(response);
+    toast.success("Successfully create new Account!");
+    form.resetFields();
+    handleCancel();
+    fetchAccounts();
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      await api.delete(`/authentication/delete-account/${id}`);
+      message.success("account deleted successfully");
+      const response = await api.get(`/authentication/account`);
+      if (response.data.deleted) {
+        // Nếu sản phẩm đã được đánh dấu là đã xóa thì không hiển thị nó trên màn hình
+        return;
+      }
+      fetchAccounts();
+    } catch (error) {
+      console.error("Error deleting account:", error);
+      message.error("Failed to delete acount");
+    }
+  };
+
+  /////////////////////////////
+  return (
+    <div>
+      <Button onClick={showModal} type="primary" className="">
+        Add
+      </Button>
+      {/* <button onClick={handleDeleteClick}>Delete</button> */}
+      <Table
+        className="p-2"
+        columns={columns}
+        dataSource={accounts}
+        onChange={onChange}
+      />
+      <Modal
+        title="Thêm tài khoản"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <Form
+          onFinish={onSubmit}
+          labelCol={{
+            span: 24,
+          }}
+          form={form}
+        >
+          <Form.Item
+            label="Name"
+            name={"username"}
+            rules={[
+              {
+                required: true,
+                message: "Please input your name!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name={"password"}
+            rules={[
+              {
+                required: true,
+                message: "Please input password!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Email"
+            name={"email"}
+            rules={[
+              {
+                required: true,
+                message: "Please input email!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Phone"
+            name={"phone"}
+            rules={[
+              {
+                required: true,
+                message: "Please input your phone number!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Adress"
+            name={"address"}
+            rules={[
+              {
+                required: true,
+                message: "Please input your adress!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+        </Form>
+      </Modal>
+    </div>
+  );
+};
+export default ManageAccount;
--- src/main.jsx
+++ src/main.jsx
@@ -17,13 +17,14 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./home/Home.jsx";
 import Blog from "./blog/Blog.jsx";
 import Shop from "./shop/Shop.jsx";
-import Login from "./components/Login.jsx"
-import Signup from "./components/Signup.jsx"
+import Login from "./components/Login.jsx";
+import Signup from "./components/Signup.jsx";
 import { AuthProvider } from "./utilis/AuthProvider.jsx";
 import SingleProduct from "./shop/SingleProduct.jsx";
 import CartPage from "./shop/CartPage.jsx";
 import Contact from "./home/Contact.jsx";
-
+import Dashboard from "./components/DashBoard/index.jsx";
+import ManageAccount from "./components/DashBoard/Admin/ManageAccount.jsx";
 
 const router = createBrowserRouter([
   {
@@ -45,20 +46,53 @@ const router = createBrowserRouter([
       },
       {
         path: "/sign-up",
-        element: <Signup />
+        element: <Signup />,
       },
       {
         path: "/shop/:productId",
-        element: <SingleProduct />
+        element: <SingleProduct />,
       },
       {
         path: "/cart-page",
-        element: <CartPage />
+        element: <CartPage />,
       },
       {
         path: "/contact",
-        element: <Contact />
-      }
+        element: <Contact />,
+      },
+    ],
+  },
+  {
+    path: "/Dashboard",
+    children: [
+      {
+        // admin
+        path: "admin",
+        element: <Dashboard role={"ADMIN"} />,
+        children: [
+          {
+            path: "account",
+            element: <ManageAccount />,
+          },
+          // {
+          //   path: "product",
+          //   element: <ManageProduct />,
+          // },
+          // {
+          //   path: "category",
+          //   element: <ManageCategory />,
+          // },
+        ],
+      },
+      // admin
+      //////////////////////////////
+      // Assessor
+      {
+        path: "assessor",
+        element: <Dashboard role={"ASSESSOR"} />,
+        children: [{}],
+        // Assessor
+      },
     ],
   },
 ]);
